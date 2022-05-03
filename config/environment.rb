d3082ee8f7e447430fbf29e4f13d162e0d812597
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-03 10:15:10 -0400

    commit tree f2f593aed003f28fc4f713a4a8175ce42f9e1f89

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,18 @@
-# Require statements should go here.
+require_relative '../lib/garden'
+require_relative '../lib/plant'
+
+#!/usr/bin/env ruby
+require_relative '../config/environment'
+
+lawn = Garden.new(name: 'Front Lawn')
+
+basil = Plant.new(name: 'Basil')
+basil.garden = lawn
+
+cucumber = Plant.new(name: 'Cucumber')
+cucumber.garden = lawn
+
+p lawn.plants
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
